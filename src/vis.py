eafc55eaa536d51bf37f613c427467f45ee32497
JuliusJacobitz <julius@jacobitz.de>
2025-10-21 22:24:45 +0200

    improved visualization

--- src/vis.py
+++ src/vis.py
@@ -3,8 +3,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 folder = "/Users/julius/Library/CloudStorage/GoogleDrive-juliusjacobitz@gmail.com/My Drive/Studium/Master/07_Semester_Leuven/Genetic Algorithms/CodeGroupPhase/src/data/output_julius/"
-tour= "250"
-filepath = f"{tour}/tour_{tour}_julius_1761048067.csv"
+
+######
+tour= "50"
+y_lim_min = None
+y_lim_max = 30000
+filepath = f"{tour}/tour_50_julius_1761047781.csv"
+######
+
+
 df = pd.read_csv(folder+filepath,skiprows=1)
 df.columns = [c.strip() for c in df.columns]
 
@@ -14,7 +21,7 @@ df.columns = [c.strip() for c in df.columns]
 plt.figure(figsize=(10, 6))
 plt.plot(df["# Iteration"], df["Mean value"], label="Mean Value", color='blue')
 plt.plot(df["# Iteration"], df["Best value"], label="Best Value", color='orange')
-plt.ylim(80000, 150000)
+plt.ylim(y_lim_min, y_lim_max)
 plt.xlabel("Iteration")
 plt.ylabel("Value")
 plt.title("Mean and Best Value over Iterations")
